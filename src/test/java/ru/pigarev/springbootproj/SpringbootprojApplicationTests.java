c3017b8207a01d00e8b1c6a8363b6de01dc6ea20
Stanislav <grafwwwolf@yandex.ru>
2022-05-29 18:43:26 +0300

    Initial commit

---
+++ src/test/java/ru/pigarev/springbootproj/SpringbootprojApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pigarev.springbootproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
